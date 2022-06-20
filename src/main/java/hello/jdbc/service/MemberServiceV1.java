0e76451071a90609acd2be9f48346833e17b8cb8
K-J-HYEON <kjh950601@naver.com>
2022-06-20 13:19:47 +0900

    SpringAOP

--- src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -14,12 +14,15 @@ public class MemberServiceV1 {
 
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 시작
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
 
         memberRepository.update(fromId, fromMember.getMoney() - money);
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
+        // 커밋, 롤백
+
     }
 
     private void validation(Member toMember) {
