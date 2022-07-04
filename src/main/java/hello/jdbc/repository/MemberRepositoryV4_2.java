18dafa044910b1586dd3ea161e4750fb5d0b8936
K-J-HYEON <kjh950601@naver.com>
2022-07-04 17:34:51 +0900

    SpringDB

--- src/main/java/hello/jdbc/repository/MemberRepositoryV4_2.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV4_2.java
@@ -1,29 +1,39 @@
 package hello.jdbc.repository;
+
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jdbc.support.JdbcUtils;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
+
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.NoSuchElementException;
+
 /**
- * 트랜잭션 - 트랜잭션 매니저
- * DataSourceUtils.getConnection()
- * DataSourceUtils.releaseConnection()
+ * SQLExceptionTranslator 추가
  */
 @Slf4j
-public class MemberRepositoryV3 implements MemberRepositoryEx{
+public class MemberRepositoryV4_2 implements MemberRepository {
 
     private final DataSource dataSource;
+    private final SQLExceptionTranslator exTranslator;
 
-    public MemberRepositoryV3(DataSource dataSource) {
+    public MemberRepositoryV4_2(DataSource dataSource) {
 
         this.dataSource = dataSource;
+        this.exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
     }
-    public Member save(Member member) throws SQLException {
+
+    // 컴파일러가 안맞은거를 발견하고 오류를 내준다.
+    @Override
+    public Member save(Member member) {
         String sql = "insert into member(member_id, money) values(?, ?)";
         Connection con = null;
-        PreparedStatement pstmt = null;try {
+        PreparedStatement pstmt = null;
+
+        try {
             con = getConnection();
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, member.getMemberId());
@@ -31,12 +41,14 @@ public class MemberRepositoryV3 implements MemberRepositoryEx{
             pstmt.executeUpdate();
             return member;
         } catch (SQLException e) {
-            log.error("db error", e);
-            throw e;
+            throw exTranslator.translate("save", sql, e);
         } finally {
             close(con, pstmt, null);
         }
     }
+
+
+    @Override
     public Member findById(String memberId) {
         String sql = "select * from member where member_id = ?";
         Connection con = null;
@@ -55,14 +67,15 @@ public class MemberRepositoryV3 implements MemberRepositoryEx{
             } else {
                 throw new NoSuchElementException("member not found memberId=" +
                         memberId);
-            }
-        } catch (SQLException e) {
-            log.error("db error", e);
+            }} catch (SQLException e) {
+            throw exTranslator.translate("findById", sql, e);
         } finally {
             close(con, pstmt, rs);
         }
-        return null;
     }
+
+
+    @Override
     public void update(String memberId, int money) {
         String sql = "update member set money=? where member_id=?";
         Connection con = null;
@@ -74,11 +87,13 @@ public class MemberRepositoryV3 implements MemberRepositoryEx{
             pstmt.setString(2, memberId);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            log.error("db error", e);
+            throw exTranslator.translate("update", sql, e);
         } finally {
             close(con, pstmt, null);
         }
     }
+
+    @Override
     public void delete(String memberId) {
         String sql = "delete from member where member_id=?";
         Connection con = null;
@@ -88,11 +103,12 @@ public class MemberRepositoryV3 implements MemberRepositoryEx{
             pstmt.setString(1, memberId);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            log.error("db error", e);
+            throw exTranslator.translate("delete", sql, e);
         } finally {
             close(con, pstmt, null);
         }
     }
+
     private void close(Connection con, Statement stmt, ResultSet rs) {
         JdbcUtils.closeResultSet(rs);
         JdbcUtils.closeStatement(stmt);
--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -1,6 +1,8 @@
 package hello.jdbc.service;
+
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepositoryV3;
+import hello.jdbc.repository.MemberRepository;
+import hello.jdbc.repository.MemberRepositoryV5;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -11,51 +13,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.PlatformTransactionManager;
+
 import javax.sql.DataSource;
-import java.sql.SQLException;
-import static hello.jdbc.connection.ConnectionConst.*;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 /**
- * 트랜잭션 - @Transactional AOP
- */
+ * 예외 누수 문제 해결
+ * SQLException 제거
+ *
+ * MemberRepository 인터페이스 의존
+ *
+ * */
 @Slf4j
 @SpringBootTest
-class MemberServiceV3_3Test {public static final String MEMBER_A = "memberA";
+class MemberServiceV4Test {
+    public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
     public static final String MEMBER_EX = "ex";
+
     @Autowired
-    MemberRepositoryV3 memberRepository;
+    private MemberRepository memberRepository;
     @Autowired
-    MemberServiceV3_3 memberService;
+    private MemberServiceV4 memberService;
+
+    @TestConfiguration
+    static class TestConfig {
+
+        private final DataSource dataSource;
+
+        public TestConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }
+
+        @Bean
+        MemberRepository memberRepository() {
+//            return new MemberRepositoryV4_1(dataSource);
+//            return new MemberRepositoryV4_2(dataSource);
+            return new MemberRepositoryV5(dataSource);
+
+        }
+
+
+        @Bean
+        MemberServiceV4 MemberServiceV4() {
+            return new MemberServiceV4(memberRepository());
+        }
+    }
+
     @AfterEach
-    void after() throws SQLException {
+    void after() {
         memberRepository.delete(MEMBER_A);
         memberRepository.delete(MEMBER_B);
         memberRepository.delete(MEMBER_EX);
     }
-    @TestConfiguration
-    static class TestConfig {
-        @Bean
-        DataSource dataSource() {
-            return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        }
-        @Bean
-        PlatformTransactionManager transactionManager() {
-            return new DataSourceTransactionManager(dataSource());
-        }
-        @Bean
-        MemberRepositoryV3 memberRepositoryV3() {
-            return new MemberRepositoryV3(dataSource());
-        }
-        @Bean
-        MemberServiceV3_3 memberServiceV3_3() {
-            return new MemberServiceV3_3(memberRepositoryV3());
-        }
-    }@Test
+
+    @Test
     void AopCheck() {
         log.info("memberService class={}", memberService.getClass());
         log.info("memberRepository class={}", memberRepository.getClass());
@@ -66,7 +80,7 @@ class MemberServiceV3_3Test {public static final String MEMBER_A = "memberA";
 
     @Test
     @DisplayName("정상 이체")
-    void accountTransfer() throws SQLException {
+    void accountTransfer() {
 //given
         Member memberA = new Member(MEMBER_A, 10000);
         Member memberB = new Member(MEMBER_B, 10000);
@@ -83,7 +97,7 @@ class MemberServiceV3_3Test {public static final String MEMBER_A = "memberA";
     }
     @Test
     @DisplayName("이체중 예외 발생")
-    void accountTransferEx() throws SQLException {
+    void accountTransferEx() {
 //given
         Member memberA = new Member(MEMBER_A, 10000);
         Member memberEx = new Member(MEMBER_EX, 10000);
